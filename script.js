dd982a392ebdea936e2b007e669649e9a2ada024
Utkarsh Sharma <47462195+vishal30982@users.noreply.github.com>
2023-03-17 19:06:04 +0530

    Created script.js
    script.js is now created !

---
+++ script.js
@@ -0,0 +1,10 @@
+if (typeof Notification !== 'undefined') {
+  if(Notification.permission == 'default') {
+    Notification.requestPermission()
+  }
+}
+let title = 'The Title';
+let body = 'The body';
+setTimeout(() => {
+let notification = new Notification(title, {body});
+}, 10000);
